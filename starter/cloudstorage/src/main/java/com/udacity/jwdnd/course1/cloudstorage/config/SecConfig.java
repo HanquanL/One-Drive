71e2bdcca9c16d09174be8ee985887fda95c6782
Liu <hanquanl0115@gmail.com>
2023-11-05 17:08:43 -0500

    modified step 1.2 security config

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecConfig.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecConfig.java
@@ -2,24 +2,23 @@ package com.udacity.jwdnd.course1.cloudstorage.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration // This annotation tells Spring Boot that this is a configuration class.
+@EnableWebSecurity // This annotation tells Spring Boot to enable web security.
 public class SecConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/signup", "/css/**", "/js/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login") // This method tells Spring Boot to generate a login page for us.
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+       http.authorizeRequests()
+               .antMatchers("/signup", "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated();
 
+       http.formLogin()
+               .loginPage("/login")
+               .permitAll();
+
+       http.formLogin()
+                .defaultSuccessUrl("/home", true);
     }
 }
