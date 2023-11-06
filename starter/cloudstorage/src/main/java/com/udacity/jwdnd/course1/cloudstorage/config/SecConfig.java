dc6de8d0e47985b4c5f08892798d47e737fbcaf7
Liu <hanquanl0115@gmail.com>
2023-11-05 19:22:16 -0500

    init step 1.3 custom AuthenticationProvider

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecConfig.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecConfig.java
@@ -1,6 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
+import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration // This annotation tells Spring Boot that this is a configuration class.
 @EnableWebSecurity // This annotation tells Spring Boot to enable web security.
 public class SecConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationService authenticationService;
+
+    public SecConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(this.authenticationService);
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http.authorizeRequests()
