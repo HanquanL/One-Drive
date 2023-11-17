61f5311184975aa16117b3551a0b0e3952644aa5
Liu <hanquanl0115@gmail.com>
2023-11-16 22:23:13 -0500

    add jpa to note entity and file entiy

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/entity/Note.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/entity/Note.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer noteid;
     private String notetitle;
     private String notedescription;
